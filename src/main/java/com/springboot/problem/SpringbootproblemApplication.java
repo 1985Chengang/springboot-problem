ff83731371f30f89e5e7692b37754cc003108c0c
chengang <chengangyunnan@qq.com>
2018-10-01 15:37:56 +0800

    spring boot plus problem structure

--- src/main/java/com/springboot/problem/SpringbootproblemApplication.java
+++ src/main/java/com/springboot/problem/SpringbootproblemApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootproblemApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringbootproblemApplication.class, args);
 	}
 }
