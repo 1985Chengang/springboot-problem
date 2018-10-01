3066df604d7309be458761b461ea9d533c6a6f8f
chengang <chengangyunnan@qq.com>
2018-10-01 15:34:17 +0800

    spring boot plus problem structure

---
+++ src/main/java/com/springboot/problem/SpringbootproblemApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.problem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootproblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootproblemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/springboot/problem/SpringbootproblemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springboot.problem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootproblemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
